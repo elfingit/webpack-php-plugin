9f6303e5ce8370bc66bf41f64f2bd7f03a8e4b51
Andrey Sergeev <andreylong@gmail.com>
2020-12-22 12:46:39 +0300

    in progress

--- src/index.js
+++ src/index.js
@@ -21,7 +21,47 @@ class PhpPlugin {
             context: true
         }, (context, compiler, callback) => {
             const reportProgress = context && context.reportProgress;
-            if (reportProgress) reportProgress(0.95, 'Starting work');
+            if (reportProgress) reportProgress(0, 'Starting work');
+
+            let promises = [];
+
+            let step_progress = 100 / files.length;
+            let step = 1;
+
+            files.forEach((file) => {
+
+                let srcFilePath = self.options.srcDir + '/' + file;
+                let dstFilePath = self.options.dstDir + '/' + file;
+                let vars = self.options.vars
+
+                promises.push(new Promise((resolve, reject) => {
+                        fs.readFile(srcFilePath, (err, data) => {
+                            if (err) return reject(err);
+
+                            let content = data.toString('utf-8');
+
+                            resolve(content)
+                        });
+                    }).then((content) => {
+
+                        for (const [key, value] of Object.entries(vars)) {
+
+                            content = content.split('{' + key + '}').join(value)
+                        }
+
+                        return new Promise((resolve, reject) => {
+                            fs.writeFile(dstFilePath, content, (err) => {
+                                if (err) return reject(err)
+                                resolve()
+                            })
+                        })
+                    }).then(() => {
+
+                    }).catch((err) => {
+                        throw err;
+                    })
+                );
+            });
         });
     }
 }
